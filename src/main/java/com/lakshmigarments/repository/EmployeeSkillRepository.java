cf0fb0551d3c150e433f20e8bab168fdc663d56f
niresh <k.niresh@in.bosch.com>
2025-09-27 17:51:46 +0530

    added models and repository for employee mngt

---
+++ src/main/java/com/lakshmigarments/repository/EmployeeSkillRepository.java
@@ -0,0 +1,9 @@
+package com.lakshmigarments.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lakshmigarments.model.EmployeeSkill;
+
+public interface EmployeeSkillRepository extends JpaRepository<EmployeeSkill, Long> {
+
+}
